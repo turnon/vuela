b60cb007301b6840579047777c9bbd2a93576366
ken <block24block@gmail.com>
2020-01-27 21:50:31 +0800

    pick or drop from keywords list

--- src/components/selector.vue
+++ src/components/selector.vue
@@ -2,9 +2,9 @@
   <div>
     <ul v-for="(buckets, field) in aggs">
       <li>
-        {{ field }}
+        {{ field }} {{ selected[field] }}
         <ul v-for="b in buckets">
-          <li>{{ b.key }}({{ b.doc_count }})</li>
+          <li @click="pick_or_drop(field, b)">{{ b.key }}({{ b.doc_count }})</li>
         </ul>
       </li>
     </ul>
@@ -14,7 +14,36 @@
 <script>
   export default {
     name: 'selector',
-    props: ['aggs']
+    props: ['aggs'],
+    data() {
+      return {
+        selected: {}
+      }
+    },
+    methods: {
+      pick_or_drop(field, b) {
+        if (!this.selected[field]) {
+          this.$set(this.selected, field, {})
+        }
+        let values = this.selected[field],
+          value = values[b.key]
+
+        if (value) {
+          if (value.picked) {
+            value.picked = false
+          } else {
+            this.$delete(values, b.key)
+          }
+        } else {
+          this.$set(values, b.key, {
+            bucket: b,
+            picked: true
+          })
+        }
+
+        console.log(field, b)
+      }
+    }
   }
 </script>
 
