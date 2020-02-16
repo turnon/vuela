9ba335b773c5fe9f8f32752617a4e52cda903a37
ken <block24block@gmail.com>
2020-02-16 22:31:17 +0800

    appended buttons has height as input

--- src/components/conditions_list.vue
+++ src/components/conditions_list.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="vuela-options">
 
-    <div v-for="cond in $store.getters.conditions" :key="cond.id" style="margin-top: .25rem">
-      <component :is="cond.operator" @change_cond="change_cond('put', {id: cond.id, ...$event})" :style="{width: 'calc(100% - 97px)'}" />
+    <div v-for="cond in $store.getters.conditions" :key="cond.id" style="margin-top: .25rem; position: relative;">
+      <component :is="cond.operator" @change_cond="change_cond('put', {id: cond.id, ...$event})" :style="{width: 'calc(100% - 79px)'}" />
 
       <div class="vuela-rm-buttons">
         <el-button type="info" plain :icon="$store.getters.anti_icon(cond)" @click="change_cond('anti', cond.id)" />
@@ -61,16 +61,29 @@
   .vuela-rm-buttons {
     display: inline-block;
     margin-left: -4px;
+    position: absolute;
+    top: 0;
+    bottom: 0
+  }
+
+  .vuela-rm-buttons button {
+    border-bottom-left-radius: 0px;
+    border-top-left-radius: 0px;
+    height: 100%;
+    width: 25px;
+  }
+
+  .vuela-rm-buttons button:nth-child(1) {
+    margin-left: -1px;
+    border-bottom-right-radius: 0px;
+    border-top-right-radius: 0px;
   }
 
   .vuela-rm-buttons button:nth-child(2) {
-    margin-left: -5px;
-    width: 50px;
-    border-bottom-left-radius: 0px;
-    border-top-left-radius: 0px;
+    margin-left: -1px;
   }
 
-  .vuela-rm-buttons button:nth-child(2) i {
-    margin-left: -3px;
+  .vuela-rm-buttons button i {
+    margin-left: -8px;
   }
 </style>
